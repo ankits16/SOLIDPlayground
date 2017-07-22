8669d87d904d0b48416294553384f7cbba2d79a0
ankit <ankit.sachan16@gmail.com>
2017-07-22 10:57:24 +0530

    initial draft

---
+++ SOLIDPlayground/NotSOLIDTimeLineService.swift
@@ -0,0 +1,9 @@
+//
+//  NotSOLIDTimeLineService.swift
+//  SOLIDPlayground
+//
+//  Created by ankit on 22/07/17.
+//  Copyright © 2017 test. All rights reserved.
+//
+
+import Foundation
---
+++ SOLIDPlayground/SOLIDTimeLineService.swift
@@ -0,0 +1,9 @@
+//
+//  SOLIDTimeLineService.swift
+//  SOLIDPlayground
+//
+//  Created by ankit on 22/07/17.
+//  Copyright © 2017 test. All rights reserved.
+//
+
+import Foundation
